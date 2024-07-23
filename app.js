fc195b670075d2786775e9071cd025bd8ad892d1
devanidarshan <130140985+devanidarshan@users.noreply.github.com>
2024-07-23 08:53:00 +0530

    Task

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const app = express();
 app.use (express.json());
 const port = process.env.PORT;
 
-// app.listen(port , async() => {                                       // Online Database
-//     mongoose.connect(process.env.MONGO_DB_URL)
+// app.listen(port , async() => {                                       
+//     mongoose.connect(process.env.MONGO_DB_URL)                      // Online Database
+//     mongoose.connect('mongodb://127.0.0.1:27017/project')           // Local Database
+
 //     .then(() => console.log('DB is Connected..'))
 //     .catch(err => console.log(err.message));
 //     console.log('Server start at http://localhost:7777');
 // });
+
+
+// ROUTES:-     
+
+// Admin Routes
+const usersRoute = require('./Src/Routes/index.routes');
+app.use('/api/user', usersRoute);
 
 async function main() {
     mongoose.connect(process.env.MONGO_DB_URL)
